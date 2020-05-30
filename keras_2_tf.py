b0cb1b6f024843c4d4ae63f492ab57ddae076e64
Gokula Krishnan Ravi <gkris4062@gmail.com>
2020-05-30 21:34:03 +0530

    Update keras_2_tf.py

--- keras_2_tf.py
+++ keras_2_tf.py
@@ -1,40 +1,3 @@
-'''
- Copyright 2020 Xilinx Inc.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
-     http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-'''
-
-
-'''
- load Keras saved model in JSON and/or HDF5 format and create TensorFlow checkpoint and 
- inference graph.
-
- Usage:
-
-   If Keras model is contained in a JSON file and HDF5 file, then the paths to these files
-   should be provided using the --keras_json and --keras_hdf5 arguments.
-
-   If no --keras_json argument is provided, then the script assumes that the network architecture 
-   and weights are all contained in the HDF5 file provided by the --keras_hdf5 arguments.
-
-   The TensorFlow graph will be created in either binary or text format according to the filename 
-   provided in the --tf_graph argument.  A text format graph will be created for a .pbtxt filename extension 
-   and a binary format graph will be created for any other filename extension.
-
-   If the --tf_graph and --tfckpt arguments contain paths with folder names that do not exist, then the 
-   folders will be created. If the files already exist, they will be overwritten.
-'''
-
 import os
 import argparse
 import sys
@@ -53,9 +16,6 @@ def keras2tf(keras_json,keras_hdf5,tfckpt,tf_graph):
     # set learning phase for no training
     backend.set_learning_phase(0)
 
-    # if name of JSON file provided as command line argument, load from 
-    # arg.keras_json and args.keras_hdf5.
-    # if JSON not provided, assume complete model is in HDF5 format
     if (keras_json != ''):
         json_file = open(keras_json, 'r')
         loaded_model_json = json_file.read()
@@ -104,10 +64,8 @@ def keras2tf(keras_json,keras_hdf5,tfckpt,tf_graph):
     return
 
 
-
-# only used if script is run as 'main' from command lime
 def main():
-  # construct the argument parser and parse the arguments
+
     ap = argparse.ArgumentParser()
 
     ap.add_argument('-kj', '--keras_json',
